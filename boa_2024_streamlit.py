07994974580008b03e6683f339f4e5cbe742103f
bandScores <marchingartsbtn@gmail.com>
2024-10-01 22:26:58 -0400

    Update boa_2024_streamlit.py

--- boa_2024_streamlit.py
+++ boa_2024_streamlit.py
@@ -93,7 +93,7 @@ gridOptions = {
         'resizable': True},
     'domLayout': 'autoHeight',
     'pagination': True,
-    'paginationPageSize': 150,
+    'paginationPageSize': 25,
     'cellStyle': {'fontSize': '5px'},
     'columnDefs': [
         {'headerName': 'Date', 'field': 'Date', 'width': 90, 'filter': 'true'},
