5184520c8e16052deeea7da2458fa1cde8459f82
bandScores <marchingartsbtn@gmail.com>
2024-10-02 22:27:29 -0400

    Update boa_2024_streamlit.py

--- boa_2024_streamlit.py
+++ boa_2024_streamlit.py
@@ -68,22 +68,6 @@ with row_input[1]:
 #with row_input[2]:
 freeze = st.radio('Freeze Date, Event, Round, School, and Class columns', ['Yes', 'No (recommended for mobile users)'])
 
-
-# display = display.style.format({'MPI': '{:^20.3f}', 'MPI Rank': '{:^20.0f}', 
-#                                   'MPE': '{:^20.3f}',  'MPE Rank': '{:^20.0f}',  
-#                                   'Mus Avg': '{:^20.3f}',  'Mus Avg Rank': '{:^20.0f}', 
-#                                   'VPI': '{:^20.3f}',  'VPI Rank': '{:^20.0f}',  
-#                                   'VPE': '{:^20.3f}',  'VPE Rank': '{:^20.0f}',  
-#                                   'Vis Avg': '{:^20.3f}',  'Vis Avg Rank': '{:^20.0f}', 
-#                                   'MGE1':'{:^20.3f}',  'MGE1 Rank':'{:^20.0f}',  
-#                                   'MGE2':'{:^20.3f}',  'MGE2 Rank':'{:^20.0f}',  
-#                                   'MGE Tot':'{:^20.3f}',  'MGE Tot Rank':'{:^20.0f}', 
-#                                   'VGE':'{:^20.3f}',  'VGE Rank':'{:^20.0f}',  
-#                                   'GE Tot':'{:^20.3f}',  'GE Tot Rank':'{:^20.0f}',  
-#                                   'Subtotal':'{:^20.3f}',  #'Subtotal Rank',
-#                                   'Pen': '{:^20.0f}',  'Total': '{:^20.3f}',  
-#                                   'Place: Class': '{:^20.0f}',  'Place: Overall': '{:^20.0f}'})
-
 gridOptions = {
     'defaultColDef': {
         'sortable': True,
@@ -181,84 +165,6 @@ if event is None:
 
 if round is None:
             gridOptions['columnDefs'][2].pop('hide', None)
-
-
-# date_col = {'headerName': 'Date', 'field': 'Date', 'maxWidth':85, 'pinned': 'left', 'filter': 'true'}
-# event_col = {'headerName': 'Event', 'field': 'Event', 'autoWidth':'True', 'pinned': 'left', 'filter': 'true'}
-# round_col = {'headerName': 'Round', 'field': 'Round', 'maxWidth':80, 'pinned': 'left', 'filter': 'true'}
-# school_col = {'headerName': 'School', 'field': 'School', 'autoWidth':'True', 'pinned': 'left', 'filter': 'true'}
-# class_col = {'headerName': 'Class', 'field': 'Class', 'maxWidth':70, 'pinned': 'left', 'filter': 'true'}
-
-# cols1 = [date_col, event_col, round_col, school_col, class_col] #all cols
-# cols2 = [round_col, school_col, class_col] #event selected round empty
-# cols3 = [date_col, event_col, school_col, class_col] #round selected event empty
-# cols4 = [school_col, class_col] #round and event selected
-
-# if event is None and round is None:
-#     if freeze == 'Yes':
-#         for i in range(len(cols1)):
-#             cols1[i]['pinned'] = 'left'
-#             gridOptions['columnDefs'].insert(i, cols1[i])
-#     if freeze != 'Yes':
-#         for i in range(len(cols1)):
-#             gridOptions['columnDefs'].insert(i, cols1[i])
-
-# if event is not None and round is None:
-#     if freeze == 'Yes':
-#         for i in range(len(cols2)):
-#             cols2[i]['pinned'] = 'left'
-#             gridOptions['columnDefs'].insert(i, cols2[i])
-#     if freeze != 'Yes':
-#         for i in range(len(cols2)):
-#             gridOptions['columnDefs'].insert(i, cols2[i])
-
-# if event is None and round is not None:
-#     if freeze == 'Yes':
-#         for i in range(len(cols3)):
-#             cols3[i]['pinned'] = 'left'
-#             gridOptions['columnDefs'].insert(i, cols3[i])
-#     if freeze != 'Yes':
-#         for i in range(len(cols3)):
-#             gridOptions['columnDefs'].insert(i, cols3[i])
-
-# if event is not None and round is not None:
-#     if freeze == 'Yes':
-#         for i in range(len(cols4)):
-#             cols4[i]['pinned'] = 'left'
-#             gridOptions['columnDefs'].insert(i, cols4[i])
-#     if freeze != 'Yes':
-#         for i in range(len(cols4)):
-#             gridOptions['columnDefs'].insert(i, cols4[i])
-
-
-# if freeze == 'Yes':
-#             if event is None and round is None:
-                        
-#             gridOptions['columnDefs'][0] = {'headerName': 'Date', 'field': 'Date', 'maxWidth':85, 'pinned': 'left', 'filter': 'true'}
-#             gridOptions['columnDefs'][1] = {'headerName': 'Event', 'field': 'Event', 'autoWidth':'True', 'pinned': 'left', 'filter': 'true'}
-#             gridOptions['columnDefs'][2] = {'headerName': 'Round', 'field': 'Round', 'maxWidth':80, 'pinned': 'left', 'filter': 'true'}
-#             gridOptions['columnDefs'][3] = {'headerName': 'School', 'field': 'School', 'autoWidth':'True', 'pinned': 'left', 'filter': 'true'}
-#             gridOptions['columnDefs'][4] = {'headerName': 'Class', 'field': 'Class', 'maxWidth':70, 'pinned': 'left', 'filter': 'true'}
-
-# if freeze == 'No (recommended for mobile users)':
-#             gridOptions['columnDefs'][0] = {'headerName': 'Date', 'field': 'Date', 'maxWidth':85, 'filter': 'true'}
-#             gridOptions['columnDefs'][1] = {'headerName': 'Event', 'field': 'Event', 'autoWidth':'True', 'filter': 'true'}
-#             gridOptions['columnDefs'][2] = {'headerName': 'Round', 'field': 'Round', 'maxWidth':80, 'filter': 'true'}
-#             gridOptions['columnDefs'][3] = {'headerName': 'School', 'field': 'School', 'autoWidth':'True', 'filter': 'true'}
-#             gridOptions['columnDefs'][4] = {'headerName': 'Class', 'field': 'Class', 'maxWidth':7, 'filter': 'true'}
-
-# if round == 'Finals':
-#     gridOptions['columnDefs'][-1]= {'headerName': 'Final Ranks','children': [
-#         {'headerName': 'Overall.','field': 'Place: Overall','width': 90,
-#          'headerClass': 'left-header', 'cellStyle': {'textAlign': 'center'},'type': 'numericColumn'}]}
-# if event is not None:
-#     gridOptions['columnDefs'].remove({'headerName': 'Date', 'field': 'Date', 'maxWidth':85, 'pinned': 'left', 'filter': 'true'})
-#     gridOptions['columnDefs'].remove({'headerName': 'Event', 'field': 'Event', 'autoWidth':'True', 'pinned': 'left', 'filter': 'true'})
-# if round is not None:
-#     gridOptions['columnDefs'].remove({'headerName': 'Round', 'field': 'Round', 'maxWidth':80, 'pinned': 'left', 'filter': 'true'})
-
-
-
 
 # st.markdown(
 #     """
