22269d172aca43eb8871c3259401e1469f98119c
bandScores <marchingartsbtn@gmail.com>
2024-10-03 17:02:27 -0400

    Update boa_2024_streamlit.py

--- boa_2024_streamlit.py
+++ boa_2024_streamlit.py
@@ -124,6 +124,7 @@ gridOptions = {
     #'domLayout': 'autoHeight',
     'pagination': True,
     'paginationPageSize': 100,
+    'headerHeight':150,
     'cellStyle': {'fontSize': '5px'},
     'columnDefs': [
         {'headerName': 'Date', 'field': 'Date', 'width':85, 'pinned':'left', 'filter': 'true', 'hide':'true'}, #85
@@ -132,7 +133,7 @@ gridOptions = {
         {'headerName': 'School', 'field': 'School', 'width':180, 'pinned':'left', 'filter': 'true'}, #auto
         {'headerName': 'Class', 'field': 'Class', 'width':70, 'pinned':'left', 'filter': 'true', 'hide':'true', "cellStyle": {"border-right": "4px solid #FF0000"}}, #70
         
-        {'headerName': 'Music Perf.\nInd.', 'field': 'MPI', 'width':90, 'headerHeight':150,
+        {'headerName': 'Music Perf.\nInd.', 'field': 'MPI', 'width':90,
          "valueGetter": "data.MPI.toFixed(3) + '  (' + data.MPI_Rank + ')'",  # Combine Score and Rank into one string
          "sortable": True,
          "sortComparator": "function(a, b, nodeA, nodeB, isInverted) { return nodeA.data.MPI - nodeB.data.MPI; }"},
