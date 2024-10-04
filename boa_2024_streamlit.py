8934b4588e113c352105a281576827ad2568bec7
bandScores <marchingartsbtn@gmail.com>
2024-10-03 21:17:41 -0400

    Update boa_2024_streamlit.py

--- boa_2024_streamlit.py
+++ boa_2024_streamlit.py
@@ -37,7 +37,7 @@ markdown = """
         justify-content: center !important;  /* Center align the header text */
     }
 
-    ag-header-cell-label {
+    .custom-header .ag-header-cell-label {
         justify-content: center !important;  /* Center align the header text */
     }
     
@@ -143,16 +143,16 @@ gridOptions = {
         {'headerName': 'School', 'field': 'School', 'width':180, 'pinned':'left', 'filter': 'true'}, #auto
         {'headerName': 'Class', 'field': 'Class', 'width':70, 'pinned':'left', 'filter': 'true', 'hide':'true', "cellStyle": {"border-right": "4px solid #FF0000"}}, #70
         
-        {'headerName': 'Music Ind.', 'field': 'MPI', 'width':90, 'wrapHeaderText': 'true', "cellStyle": {"text-align": "center", "textAlign": "center"},
+        {'headerName': 'Music Ind.', 'field': 'MPI', 'width':90, 'wrapHeaderText': 'true', "cellStyle": {"text-align": "center"}, "headerClass": "custom-header", 
          "valueGetter": "data.MPI.toFixed(3) + '  (' + data.MPI_Rank + ')'",  # Combine Score and Rank into one string
          "sortable": True,
          "sortComparator": "function(a, b, nodeA, nodeB, isInverted) { return nodeA.data.MPI - nodeB.data.MPI; }"},
-        {'headerName': 'Music Ens.', 'field': 'MPE', 'width':90, 'wrapHeaderText': 'true', "cellStyle": {"text-align": "center", "textAlign": "center"},
+        {'headerName': 'Music Ens.', 'field': 'MPE', 'width':90, 'wrapHeaderText': 'true', "cellStyle": {"text-align": "center"}, "headerClass": "custom-header"
          "valueGetter": "data.MPE.toFixed(3) + '  (' + data.MPE_Rank + ')'",  # Combine Score and Rank into one string
          "sortable": True,
          "sortComparator": "function(a, b, nodeA, nodeB, isInverted) { return nodeA.data.MPE - nodeB.data.MPE; }"},     
-        {'headerName': 'Music Average', 'field': 'Mus_Avg', 'width':90, 'wrapHeaderText': 'true', "cellStyle": {"text-align": "center", "textAlign": "center", "border-right": "4px solid #FF0000"}, 
-         "valueGetter": "data.Mus_Avg.toFixed(3) + '  (' + data.Mus_Avg_Rank + ')'",  # Combine Score and Rank into one string
+        {'headerName': 'Music Average', 'field': 'Mus_Avg', 'width':90, 'wrapHeaderText': 'true', "cellStyle": {"text-align": "center", "border-right": "4px solid #FF0000"}, 
+         "headerClass": "custom-header", "valueGetter": "data.Mus_Avg.toFixed(3) + '  (' + data.Mus_Avg_Rank + ')'",  # Combine Score and Rank into one string
          "sortable": True,
          "sortComparator": "function(a, b, nodeA, nodeB, isInverted) { return nodeA.data.Mus_Avg - nodeB.data.Mus_Avg; }"},
         
