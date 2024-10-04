fbc9836fd24615a0126a758922db6c1c4b207515
bandScores <marchingartsbtn@gmail.com>
2024-10-04 10:01:32 -0400

    Update boa_2024_streamlit.py

--- boa_2024_streamlit.py
+++ boa_2024_streamlit.py
@@ -90,7 +90,7 @@ gridOptions = {
     'cellStyle': {'fontSize': '5px'},
     'columnDefs': [
         {'headerName': 'Date', 'children':
-                [{'headerName':'', 'field': 'Date', 'maxWidth':85, 'pinned':'left', 'filter': 'true', 'hide':'true'}]}, #85
+                [{'headerName':'', 'field': 'Date', 'maxWidth':85, 'pinned':'left', 'filter': 'true'}], 'hide':'true'}, #85
         {'headerName': 'Event', 'field': 'Event', 'width':170, 'pinned':'left', 'filter': 'true', 'hide':'true'}, #auto
         {'headerName': 'Round', 'field': 'Round', 'maxWidth':80, 'pinned':'left', 'filter': 'true', 'hide':'true'}, #80
         {'headerName': 'School', 'field': 'School', 'width':190, 'pinned':'left', 'filter': 'true'}, #auto
