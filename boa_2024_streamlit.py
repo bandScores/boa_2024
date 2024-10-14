ea0aafa9c2da99781795d1f4d39e7a44cc5e4f2c
bandScores <marchingartsbtn@gmail.com>
2024-10-14 13:22:12 -0400

    Update boa_2024_streamlit.py

--- boa_2024_streamlit.py
+++ boa_2024_streamlit.py
@@ -17,7 +17,7 @@ st.set_page_config(layout="wide")
 
 url = "https://docs.google.com/spreadsheets/d/1dgV0saovQ5tVe6OeGYHLaLMzh-2qq1EfUVGfQg3XRhU/edit?usp=sharing"
 conn = st.connection("gsheets", type=GSheetsConnection)
-raw = conn.read(spreadsheet=url)
+raw = conn.read()
 
 #raw = pd.read_csv('boa_2024_oct12.csv')
 raw = raw.rename(columns={'P/S/F': 'Round', 'School Full':'School'})
