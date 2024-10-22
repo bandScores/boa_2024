2e05308bb839f28f70ba35f3b4279d43e0d84915
bandScores <marchingartsbtn@gmail.com>
2024-10-22 12:30:24 -0400

    Update boa_2024_streamlit.py

--- boa_2024_streamlit.py
+++ boa_2024_streamlit.py
@@ -311,8 +311,8 @@ st.markdown(
     .block-container {
         padding-top: 5rem;
         padding-bottom: 3rem;
-        padding-left: 1rem;
-        padding-right: 1rem;
+        padding-left: 5rem;
+        padding-right: 5rem;
     }
     
     /* Adjust the spacing between checkboxes */
