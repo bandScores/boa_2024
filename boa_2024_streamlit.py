4fed076281b9f5417b9340a9e33902d08025633e
bandScores <marchingartsbtn@gmail.com>
2024-10-22 08:23:39 -0400

    Update boa_2024_streamlit.py

--- boa_2024_streamlit.py
+++ boa_2024_streamlit.py
@@ -116,6 +116,7 @@ gridOptions = {
     #'domLayout': 'autoHeight',
     'pagination': True,
     'paginationPageSize': 100,
+    'rowHeight': 120,
     'cellStyle': {'fontSize': '5px'},
     'columnDefs': [
         {'headerName': 'Date', 'field': 'Date', 'maxWidth':90, 'filter': 'true', 'pinned':'left', 'hide':'true', 'headerClass': 'parent-header-left', 'suppressMovable':'true'}, #85
